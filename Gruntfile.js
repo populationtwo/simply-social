c8e19bf56a22ed496dbcec7bfe07730c14dae3c2
Edwin Lunandy <populationtwo@gmail.com>
2015-02-27 17:02:24 -0500

    Update grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,53 +2,87 @@ module.exports = function (grunt) {
 	'use strict';
 
 	// Load all grunt tasks
-	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+	require( 'matchdep' ).filterDev( 'grunt-*' ).forEach( grunt.loadNpmTasks );
 
 	// Project configuration
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+	grunt.initConfig( {
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		concat: {
+			dist: {
+				src : ['assets/js/src/*.js', 'assets/js/*.js'],
+				dest: 'assets/js/build/production.js'
+			}
+		},
+
+		jshint: {
+			all: ['assets/js/*.js']
+		},
+
+		uglify: {
+			build: {
+				src : 'assets/js/build/production.js',
+				dest: 'assets/js/build/production.min.js'
+			}
+		},
+
+		imagemin: {
+			dynamic: {
+				files: [{
+					expand: true,
+					cwd   : 'assets/img/',
+					src   : ['**/*.{png,jpg,gif}'],
+					dest  : 'assets/img/build/'
+				}]
+			}
+		},
 
 		compass: {
 			dist: {
 				options: {
-					sassDir: 'assets/scss',
-					cssDir : 'assets/css',
-					outputStyle : 'compressed'
+					sassDir    : 'assets/scss',
+					cssDir     : 'assets/css',
+					outputStyle: 'compressed'
 				}
 			}
 		},
 
-		cssmin: {
-			options: {
-				banner: '/*! <%= pkg.title %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
-				' * <%= pkg.homepage %>\n' +
-				' * Copyright (c) <%= grunt.template.today("yyyy") %>;' +
-				' * Licensed GPLv2+' +
-				' */\n'
-			},
-			minify: {
-				expand: true,
 
-				cwd: 'css/',
-				src: ['main.css'],
-
-				dest: 'css/',
-				ext: '.min.css'
+		scsslint: {
+			allFiles: ['assets/scss/**/*.scss', 'assets/scss/*.scss'],
+			options : {
+				bundleExec    : true,
+				colorizeOutput: true
 			}
 		},
-		watch  : {
-			css: {
-				files  : ['assets/scss/**/*.scss', 'assets/scss/*.scss' ],
+
+		watch: {
+			css    : {
+				files  : ['assets/scss/**/*.scss', 'assets/scss/*.scss'],
 				tasks  : ['compass'],
 				options: {
 					debounceDelay: 500
 				}
+			},
+			scripts: {
+				files  : ['assets/js/*.js'],
+				tasks  : ['concat', 'uglify', 'jshint'],
+				options: {
+					spawn: false
+				}
 			}
 		}
-	});
+	} );
+
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+	grunt.loadNpmTasks( 'grunt-contrib-imagemin' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+	grunt.loadNpmTasks( 'grunt-contrib-compass' );
+	grunt.loadNpmTasks( 'grunt-scss-lint' );
 
 	// Default task.
-	grunt.registerTask('default', ['watch']);
+	grunt.registerTask( 'default', ['concat', 'uglify', 'imagemin', 'scsslint', 'jshint'] );
 
 	grunt.util.linefeed = '\n';
 };
---
+++ assets/js/build/production.js
@@ -0,0 +1,449 @@
+/*!
+ * classie - class helper functions
+ * from bonzo https://github.com/ded/bonzo
+ * 
+ * classie.has( elem, 'my-class' ) -> true/false
+ * classie.add( elem, 'my-new-class' )
+ * classie.remove( elem, 'my-unwanted-class' )
+ * classie.toggle( elem, 'my-class' )
+ */
+
+/*jshint browser: true, strict: true, undef: true */
+/*global define: false */
+
+( function( window ) {
+
+'use strict';
+
+// class helper functions from bonzo https://github.com/ded/bonzo
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+// classList support for class management
+// altho to be fair, the api sucks because it won't accept multiple classes at once
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
+
+// Create an immediately invoked functional expression to wrap our code
+(function() {
+
+	// Define our constructor 
+	this.Modal = function() {
+
+		// Create global element references
+		this.closeButton = null;
+		this.modal = null;
+		this.overlay = null;
+
+		// Determine proper prefix
+		this.transitionEnd = transitionSelect();
+
+		// Define option defaults 
+		var defaults = {
+			autoOpen: false,
+			className: 'fade-and-drop',
+			closeButton: true,
+			content: "",
+			maxWidth: 980,
+			minWidth: 300,
+			overlay: true
+		}
+
+		// Create options by extending defaults with the passed in arugments
+		if (arguments[0] && typeof arguments[0] === "object") {
+			this.options = extendDefaults(defaults, arguments[0]);
+		}
+
+		if(this.options.autoOpen === true) this.open();
+
+	}
+
+	// Public Methods
+
+	Modal.prototype.close = function() {
+		var _ = this;
+		this.modal.className = this.modal.className.replace(" ss-open", "");
+		this.overlay.className = this.overlay.className.replace(" ss-open",
+			"");
+		this.modal.addEventListener(this.transitionEnd, function() {
+			_.modal.parentNode.removeChild(_.modal);
+		});
+		this.overlay.addEventListener(this.transitionEnd, function() {
+			if(_.overlay.parentNode) _.overlay.parentNode.removeChild(_.overlay);
+		});
+	}
+
+	Modal.prototype.open = function() {
+		buildOut.call(this);
+		initializeEvents.call(this);
+		window.getComputedStyle(this.modal).height;
+		this.modal.className = this.modal.className +
+		(this.modal.offsetHeight > window.innerHeight ?
+			" ss-open ss-anchored" : " ss-open");
+		this.overlay.className = this.overlay.className + " ss-open";
+	}
+
+	// Private Methods
+
+	function buildOut() {
+
+		var content, contentHolder, docFrag;
+
+		/*
+		 * If content is an HTML string, append the HTML string.
+		 * If content is a domNode, append its content.
+		 */
+
+		if (typeof this.options.content === "string") {
+			content = this.options.content;
+		} else {
+			content = this.options.content.innerHTML;
+		}
+
+		// Create a DocumentFragment to build with
+		docFrag = document.createDocumentFragment();
+
+		// Create modal element
+		this.modal = document.createElement("div");
+		this.modal.className = "ss-modal " + this.options.className;
+		this.modal.style.minWidth = this.options.minWidth + "px";
+		this.modal.style.maxWidth = this.options.maxWidth + "px";
+
+		// If closeButton option is true, add a close button
+		if (this.options.closeButton === true) {
+			this.closeButton = document.createElement("button");
+			this.closeButton.className = "ss-close close-button";
+			this.closeButton.innerHTML = "&times;";
+			this.modal.appendChild(this.closeButton);
+		}
+
+		// If overlay is true, add one
+		if (this.options.overlay === true) {
+			this.overlay = document.createElement("div");
+			this.overlay.className = "ss-overlay " + this.options.className;
+			docFrag.appendChild(this.overlay);
+		}
+
+		// Create content area and append to modal
+		contentHolder = document.createElement("div");
+		contentHolder.className = "ss-content";
+		contentHolder.innerHTML = content;
+		this.modal.appendChild(contentHolder);
+
+		// Append modal to DocumentFragment
+		docFrag.appendChild(this.modal);
+
+		// Append DocumentFragment to body
+		document.body.appendChild(docFrag);
+
+	}
+
+	function extendDefaults(source, properties) {
+		var property;
+		for (property in properties) {
+			if (properties.hasOwnProperty(property)) {
+				source[property] = properties[property];
+			}
+		}
+		return source;
+	}
+
+	function initializeEvents() {
+
+		if (this.closeButton) {
+			this.closeButton.addEventListener('click', this.close.bind(this));
+		}
+
+		if (this.overlay) {
+			this.overlay.addEventListener('click', this.close.bind(this));
+		}
+
+	}
+
+	function transitionSelect() {
+		var el = document.createElement("div");
+		if (el.style.WebkitTransition) return "webkitTransitionEnd";
+		if (el.style.OTransition) return "oTransitionEnd";
+		return 'transitionend';
+	}
+
+}());
+
+(function (window, $, undefined) {
+
+	// Declare all variables
+	var bodyElement = document.body,
+		contentWrap = document.querySelector( '.content-wrap' ),
+		toggleMobileMenu = document.getElementById( 'js-mobile-menu' ),
+		$mainContent = $( '.main-content' ),
+		$filterBar = $( document.getElementById( 'filterbar' ) ),
+		$allPost = $( '#partials-content' ),
+		$viewNavItem = $( '.view-nav__item' ),
+		$mobileMenuWrapper = $( '.ss-mobile-menu-wrapper' ),
+		$mobileMenuToggle = $( '#js-mobile-menu' ),
+		$html = $( 'html' ),
+		isOpen = false;
+
+
+	/**
+	 * Post Reply Toggle
+	 *
+	 * @param e
+	 */
+
+	function showReplies() {
+		$mainContent.on( 'click', ".js-btn-expand", function (e) {
+			e.preventDefault();
+			var $comments = $( this ).parent(),
+				$thisComments = $comments.siblings( ".js-post-replies" ),
+				$thisCommentsVisible = $comments.siblings( ".js-post-replies:visible" ),
+				$this = $( this );
+
+			if ($thisCommentsVisible.length) {
+				$thisComments.slideUp( 200 );
+				$this.toggleClass( 'up' );
+			} else {
+				$thisComments.slideDown( 200 );
+				$this.toggleClass( 'up' );
+			}
+		} );
+	}
+
+	/**
+	 * Load Posts from /partials.
+	 *
+	 * @param e
+	 */
+	function loadPartials() {
+		$allPost.load( "partials/all.html" );
+	}
+
+	/**
+	 * Sub navigation toggle to filter photos and videos
+	 *
+	 * @param e
+	 */
+	function filterPosts() {
+
+		$filterBar.click( function () {
+			$( ".panel" ).show().css( "display", "inline-block" );
+			$( "#filterbar a" ).parent().removeClass( "active" );
+			$( this ).find( 'li:first' ).addClass( "active" );
+			return false;
+		} );
+
+		$( ".filter" ).click( function () {
+			var thisFilter = $( this ).attr( "id" );
+			$( ".panel" ).hide();
+			$( "." + thisFilter ).fadeIn().css( "display", "inline-block" );
+			$filterBar.find( 'a' ).parent().removeClass( "active" );
+			$( this ).parent().addClass( "active" );
+			return false;
+		} );
+	}
+
+	/**
+	 * Toggle view to switch between tile and list view
+	 *
+	 * @param e
+	 */
+
+	function toggleView() {
+		$viewNavItem.on( 'click', function (e) {
+			e.preventDefault();
+			var thisView = $( this ).attr( "id" );
+			$viewNavItem.removeClass( "active" );
+			$( this ).addClass( "active" );
+			$( '#ss-all-posts' ).removeClass( 'view-list view-tile' ).addClass( thisView );
+		} );
+
+	}
+
+	/**
+	 * Modal windows
+	 *
+	 * @param e
+	 */
+	//
+	// I know it's not pretty, need to refactor these. There's a better way.
+	var myContent1 = document.getElementById( 'meg-photo' ),
+		myContent2 = document.getElementById( 'jenny-photo' ),
+		myContent3 = document.getElementById( 'buzz-photo' ),
+		myContent4 = document.getElementById( 'samihah-photo' ),
+		myContent5 = document.getElementById( 'jac-video' ),
+		myContent6 = document.getElementById( 'vitor-video' ),
+		myContent7 = document.getElementById( 'michael-video' ),
+		myContent8 = document.getElementById( 'compose-message-modal' );
+
+	var myModal1 = new Modal( {
+			content: myContent1
+		} ),
+		myModal2 = new Modal( {
+			content: myContent2
+		} ),
+		myModal3 = new Modal( {
+			content: myContent3
+		} ),
+		myModal4 = new Modal( {
+			content: myContent4
+		} ),
+		myModal5 = new Modal( {
+			content: myContent5
+		} ),
+		myModal6 = new Modal( {
+			content: myContent6
+		} ),
+		myModal7 = new Modal( {
+			content: myContent7
+		} ),
+		myModal8 = new Modal( {
+			content : myContent8,
+			maxWidth: 575
+		} );
+
+
+	$mainContent.on( 'click', "#modal-trigger-meg", function (e) {
+		e.preventDefault();
+		myModal1.open();
+	} );
+	$mainContent.on( 'click', "#modal-trigger-jenny", function (e) {
+		e.preventDefault();
+		myModal2.open();
+	} );
+	$mainContent.on( 'click', "#modal-trigger-buzz", function (e) {
+		e.preventDefault();
+		myModal3.open();
+	} );
+	$mainContent.on( 'click', "#modal-trigger-samihah", function (e) {
+		e.preventDefault();
+		myModal4.open();
+	} );
+	$mainContent.on( 'click', "#modal-trigger-jac", function (e) {
+		e.preventDefault();
+		myModal5.open();
+	} );
+	$mainContent.on( 'click', "#modal-trigger-vitor", function (e) {
+		e.preventDefault();
+		myModal6.open();
+	} );
+	$mainContent.on( 'click', "#modal-trigger-michael", function (e) {
+		e.preventDefault();
+		myModal7.open();
+	} );
+	$( '#js-compose-message, #js-compose-message-mobile' ).on( 'click', function (e) {
+		e.preventDefault();
+		myModal8.open();
+	} );
+
+	/**
+	 * Mobile Menu
+	 *
+	 * @param e
+	 */
+	function toggleCanvasMenu() {
+		if (isOpen) {
+			classie.remove( bodyElement, 'ss-mobile-menu-active' );
+		}
+		else {
+			classie.add( bodyElement, 'ss-mobile-menu-active' );
+		}
+		isOpen = !isOpen;
+	}
+
+	function initCanvasMenu() {
+		toggleMobileMenu.addEventListener( 'click', toggleCanvasMenu );
+		contentWrap.addEventListener( 'click', function (e) {
+			var target = e.target;
+			if (isOpen && target !== toggleMobileMenu) {
+				toggleCanvasMenu();
+			}
+		} );
+	}
+
+	/**
+	 * IE detection
+	 *
+	 * @param e
+	 */
+	function detectIe() {
+		"use strict";
+
+		// Detecting IE
+		var oldIE;
+		if ($html.is( '.ie6, .ie7, .ie8, .ie9' )) {
+			oldIE = true;
+		}
+		if (oldIE) {
+			$mobileMenuWrapper.hide();
+			$mobileMenuToggle.on( 'click', function (e) {
+				$mobileMenuWrapper.toggle();
+			} );
+		}
+	}
+
+	function init() {
+		loadPartials();
+		initCanvasMenu();
+		filterPosts();
+		toggleView();
+		showReplies();
+		detectIe();
+	}
+
+	init();
+})( window, jQuery, undefined );
